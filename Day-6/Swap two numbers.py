069b43a04ea4c962f58e6b5506338d1c9b69cb38
PAWAN G <121444830+pawang1745@users.noreply.github.com>
2024-03-21 10:26:47 +0530

    Swap two numbers.py
    Swap given two numbers and print them. (Try to do it without a temporary variable.) and return it.

---
+++ Day-6/Swap two numbers.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def get(self, a, b):
+        #code here
+        a=a+b
+        b=a-b
+        a=a-b
+        
+        return a, b
+
+
+if __name__ == "__main__":
+	t = int (input ())
+	for tc in range (t):
+		a,b = map(int,input().strip().split())
+		ob = Solution()
+		ans=ob.get(a,b)
+		print(str(ans[0])+" "+str(ans[1]))
