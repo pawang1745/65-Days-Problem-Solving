4e8ce5437ccaaf40c7c138cc20bb94852c9c34bc
PAWAN G <121444830+pawang1745@users.noreply.github.com>
2024-04-13 20:56:15 +0530

    Print the Kth Digit.py
    Given two numbers A and B, find Kth digit from right of AB.

---
+++ Print the Kth Digit.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kthDigit(self, A, B, K):
+        # code here
+         # Calculate A^B
+        result = A ** B
+    
+    # Convert result to string
+        result_str = str(result)
+    
+    # Find the Kth digit from right
+        return result_str[-K]
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        A,B,K = input().split()
+        ob = Solution()
+        print(ob.kthDigit(int(A),int(B),int(K)))
